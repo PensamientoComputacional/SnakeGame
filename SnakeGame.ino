3bf2544e37019ffc244c553a8cee4d8476728a58
Federico Mello <fedemello@gmail.com>
2019-03-01 18:20:26 -0300

    Se agregan funciones faltantes y se mejora la documentación

--- SnakeGame.ino
+++ SnakeGame.ino
@@ -1,4 +1,4 @@
-//We always have to include the library
+/*Incluimos la librería LedControl: */
 #include "LedControl.h"
 
 /*
@@ -9,14 +9,32 @@
   pin 10 is connected to LOAD
   We have only a single MAX72XX.
 */
-LedControl lc = LedControl(12, 11, 10, 1);
+LedControl lc = LedControl(12, 11, 10, 1); /* (DataIN, CLK, CS, Número de matrices8x8)  */
 
-int VX[] = {0, 1, 2};
-int VY[] = {0, 0, 0};
-int orientation = 1;// 0 up, 1 right, 2 down, 3 left.
+/* Puertos analógicos para la conexion del joystick:  */
+const short int joystick[2] = {0,1};
+
+/* Largo máximo para la víbora: */
+const short int snakeMaxLenght = 10;
+
+/* Largo actual de la víbora: */
+short int snakeLenght = 3;
+
+/* Variable booleana para el gameover: */
+boolean gameOver = false;
+
+/* La víbora es una lista de coordenadas [x,y] a encenderse en la matriz led: */
+short int snake[snakeMaxLenght][2] = {{0,0},{1,0},{2,0}};
+
+/* El ratón es una coordenada */
+short int mouseX;
+short int mouseY;
+
+/*Guarda la dirección hacia donde debe moverse la víbora: */
+short int orientation = 0; /* 0 right, 1 left, 2 up, 3 down. */
 
 /* we always wait a bit between updates of the display */
-unsigned long delaytime = 100;
+unsigned long delaytime = 300;
 
 void setup() {
   /*
@@ -29,63 +47,150 @@ void setup() {
   /* and clear the display */
   lc.clearDisplay(0);
 
+  randomSeed(analogRead(5)); /* Valor aleatorio para inicializar random */
+  newMouse();                /* Creramos el primer ratón */
 }
 
+/* Función que crea un nuevo ratón en una posición aleatoria: */
+void newMouse(){
+  mouseX = random(7);
+  mouseY = random(7);
+}
 
+/* Función que evalúa la posición del joystick y devuelve una orientación: */
+short int control(const short int device[2]){
+  int x = analogRead(device[0]);
+  int right = x-512;
+  int left = 512-x;
+  int y = analogRead(device[1]);
+  int up = y-512;
+  int down = 512-y;
 
+  if(orientation != 1 and right > 300 and right > left and right > up and right > down){
+    return 0;
+  }
+  else if (orientation != 0 and left > 300 and left > right and left > up and left > down){
+    return 1;
+  }
+  else if (orientation != 3 and up > 300 and up > right and up > left and up > down){
+    return 2;
+  }
+  else if (orientation != 2 and down > 300 and down > right and down > left and down > up){
+    return 3;
+  }
+  else{
+    return orientation;
+  }
+}
+
+/* Función que establece una nueva posición para la víbora: */
 void go() {
-  int lenght = sizeof(VX)/sizeof(VX[0]);
-  for (int i=(lenght-1);i>0;i--){ 
-    VX[i] = VX[(i-1)];
-    VY[i] = VY[(i-1)];
+  /* Se establecen nuevas posiciones para el cuerpo; */
+  for (int i=snakeLenght-1;i>0;i--){ /* desde la cola hasta el cuello... */
+    snake[i][0] = snake[i-1][0];     /* cada nodo ocupa la posición que ocupaba el siguiente */
+    snake[i][1] = snake[i-1][1];
    }
-  if (orientation == 0){
-   if (VY[0]==0){
-    VY[0] = 7 ;
-   }
-   else {
-    VY[0]--; 
-   }
+   
+  /* ...y una nueva posicion para la cabeza: */
+  switch(orientation){
+    case 0: {                /* Si estamos yendo a la derecha... */
+      if (snake[0][0]==7){  /* ... y ya llegamos al límite de la pantalla... */
+        snake[0][0] = 0;    /* ... reraparecemos del otro lado. */
+      }
+      else {                /* Si no hemos llegado al límite... */
+        snake[0][0]++;      /* avanzamos una celda a la derecha */
+      }
+      break;
+    }
+    case 1: {                /* Si estamos yendo a la izquierda... */
+      if (snake[0][0]==0){  /* ... y ya llegamos al límite de la pantalla... */
+        snake[0][0] = 7;    /* ... reraparecemos del otro lado. */
+      }
+      else {                /* Si no hemos llegado al límite... */
+        snake[0][0]--;      /* avanzamos una celda a la izquierda */
+      }
+      break;
+    }
+    case 2: {                /* Si estamos yendo arriba... */
+      if (snake[0][1]==0){  /* ... y ya llegamos al límite de la pantalla... */
+        snake[0][1] = 7;    /* ... reraparecemos del otro lado. */
+      }
+      else {                /* Si no hemos llegado al límite... */
+        snake[0][1]--;      /* avanzamos una celda hacia arriba */
+      }
+      break;
+    }
+    case 3: {                /* Si estamos yendo hacia abajo... */
+      if (snake[0][1]==7){  /* ... y ya llegamos al límite de la pantalla... */
+        snake[0][1] = 0;    /* ... reraparecemos del otro lado. */
+      }
+      else {                /* Si no hemos llegado al límite... */
+        snake[0][1]++;      /* avanzamos una celda hacia abajo */
+      }
+      break;
+    }
   }
-  else if (orientation == 2){
-   if (VY[0]==7){
-    VY[0] = 0 ;
-   }
-   else {
-    VY[0]++; 
-   }
+  /* Si la nueva posición de la cabeza coincide con alguna posición del cuerpo: */
+  for(int i = 3; i < snakeLenght; i++){ /* revisamos desde la posición 3 hasta la cola...*/
+    if(snake[i][0] == snake[0][0] and snake[i][1] == snake[0][1]){ /* si coincide con la cabeza...*/
+      gameOver = true;  /* termina el juego */
+    }
   }
-  if (orientation == 1){
-   if (VX[0]==7){
-    VX[0] = 0 ;
-   }
-   else {
-    VX[0]++; 
-   }
-  }
-  else if (orientation == 3){
-   if (VX[0]==0){
-    VX[0] = 7 ;
-   }
-   else {
-    VX[0]--; 
-   }
+  
+  /* Si la nueva posicion de la cabeza coincide con la posición del ratón: */
+  if(snake[0][0] == mouseX and snake[0][1] == mouseY){
+    /* Aumenta en 1 el largo de la víbora: */
+    snakeLenght++;
+    /* Y se crea un nuevo ratón: */
+    newMouse();
   }
 
+  /* Si sobrepasamos el máximo largo de la víbora: */
+  if(snakeLenght > snakeMaxLenght){
+    /* Aumenta la velocidad del juego en 40ms por loop: */
+    delaytime-= 40;
+    /* Y devuelve la víbora al largo inicial: */
+    snakeLenght = 3; 
+  }
 }
-void show(){
-  int lenght = sizeof(VX)/sizeof(VX[0]);
 
-  for (int i=0;i<lenght;i++){
-    lc.setLed(0,VX[i],VY[i], true);
+/* Función que muestra en la pantalla la víbora y el ratón: */
+void show(){
+  /*Muestra desde la cabeza a la cola todos los nodos de la víbora: */
+  for (int i=0;i<snakeLenght;i++){
+    lc.setLed(0,snake[i][0],snake[i][1], true);
+  }
+  /* Muestra el ratón: */
+  lc.setLed(0, mouseX, mouseY, true);
+}
+
+/* Pantalla de game over: */
+void gameOverScreen(){
+  for(int i = 0; i < 8; i++){
+    lc.setRow(0, i, B11111111);
+    delay(100); 
+  }
+  for(int i = 0; i < 8; i++){
+    lc.setRow(0, i, B00000000);
+    delay(100); 
   }
 }
 
 void loop() {
-
-  lc.clearDisplay(0);
-  show();
-  go();
-  
-  delay(300);
+  /*Si hemos perdido, muestra la pantala de game over: */
+  if(gameOver){
+    gameOverScreen();
+  }
+  else{  /* de lo contrario... */
+    /* actualiza la orientación de la víbora: */
+    orientation = control(joystick);
+    /* actualiza la posición de la víbora: */
+    go();
+    /* limpia la pantalla: */
+    lc.clearDisplay(0);
+    /* muestra la vibora y el ratón: */
+    show();
+    /* espera antes de reiniciar el loop: */
+    delay(delaytime);
+  }
 }
